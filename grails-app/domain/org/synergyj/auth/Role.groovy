284bb5c28aecd3d96ba653203f8e8642f56cc395
Jose Juan R. Zuñiga <neodevelop@MBPJJRZ.gateway.2wire.net>
2009-02-10 15:19:49 -0600

    Role Class implemented...

--- grails-app/domain/org/synergyj/auth/Role.groovy
+++ grails-app/domain/org/synergyj/auth/Role.groovy
@@ -1,5 +1,21 @@
 package org.synergyj.auth
 
-class Role{
-	
+import org.synergyj.auth.User
+
+/**
+ * Authority domain class.
+ */
+class Role {
+
+	static hasMany = [people: User]
+
+	/** description */
+	String description
+	/** ROLE String */
+	String authority
+
+	static constraints = {
+		authority(blank: false, unique: true)
+		description()
+	}
 }
