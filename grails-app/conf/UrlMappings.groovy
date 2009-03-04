13e37799cbbde4442392289e4c5e38b1e55bf1c4
Jose Juan R. Zuñiga <neodevelop@MBPJJRZ.local>
2009-03-04 00:48:47 -0600

    /home mapped in URL...

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,10 +1,11 @@
 class UrlMappings {
-    static mappings = {
-      "/$controller/$action?/$id?"{
-	      constraints {
-			 // apply constraints here
-		  }
-	  }
-	  "500"(view:'/error')
+	static mappings = {
+		"/$controller/$action?/$id?"{
+			constraints {
+				// apply constraints here
+			}
+		}
+		"/home"(controller:'cms',action:'index')
+		"500"(view:'/error')
 	}
 }
