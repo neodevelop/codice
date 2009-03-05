31fb576d349ed3716763438cb52aaf934fe32c5f
Jose Juan R. Zuñiga <neodevelop@MBPJJRZ.local>
2009-03-05 02:05:26 -0600

    Role and Requestmap initialized

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,16 @@
+import com.synergyj.auth.Role
+import com.synergyj.auth.RequestMap
+
 class BootStrap {
 
-     def init = { servletContext ->
-     }
-     def destroy = {
-     }
+	def init = { servletContext ->
+		new Role(authority:'ROLE_USER',description:'User CMS').save()
+		new Role(authority:'ROLE_MANAGER',description:'Manager CMS').save()
+		new Role(authority:'ROLE_ADMIN',description:'Admin CMS').save()
+		new Role(authority:'IS_AUTHENTICATED_ANONYMOUSLY',description:'Anonymous user').save()
+		new RequestMap(url:'/**',configAttribute:'IS_AUTHENTICATED_ANONYMOUSLY').save()
+		new RequestMap(url:'/content/create',configAttribute:'ROLE_USER').save()
+	}
+	def destroy = {
+	}
 } 
