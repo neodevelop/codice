7f99a6bfc7b65879b6775e58ff361cd627243e1b
Jose Juan R. Zuñiga <neodevelop@MBPJJRZ.local>
2009-03-09 05:39:24 -0600

    blank property added...

--- grails-app/controllers/com/synergyj/codice/content/ContentCommand.groovy
+++ grails-app/controllers/com/synergyj/codice/content/ContentCommand.groovy
@@ -5,22 +5,19 @@ import com.synergyj.codice.content.*
 import com.synergyj.codice.Cms
 
 
-class Content implements Serializable{
+class ContentCommand{
 	
-	User user
+	String email
 	String title
 	String body
-	Date created
-	Date lastUpdated
 	Boolean allowComments
 	Boolean publish
 	Boolean showInMainPage
-	String contentType
 	String tags
 	
 	static constraints = {
-		title nullable:false,size:5..128
-		body nullable:false,maxSize:10000
-		contentType inList:['Content','Blog','News']
+		email nullable:false,blank:false,email:true
+		title nullable:false,size:5..128,blank:false
+		body nullable:false,maxSize:10000,blank:false
 	}
 }
