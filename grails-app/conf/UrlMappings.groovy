348366002eefbfadee99334762bf3aa7ae71baa0
Jose Juan R. Zuñiga <neodevelop@MBPJJRZ.local>
2009-03-09 05:12:47 -0600

    /createContetn mapping added

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -5,9 +5,16 @@ class UrlMappings {
 				// apply constraints here
 			}
 		}
+		/***
+		Home and Register URL's
+		***/
 		"/"(controller:'cms',action:'index')
 		"/home"(controller:'cms',action:'index')
 		"/register"(controller:'register',action:'index')
+		/***
+		Content mapping
+		***/
+		"/createContent"(controller:'content',action:'create')
 		"500"(view:'/error')
 	}
 }
