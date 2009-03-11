1e420a8c4b84ac8ae0de80566a983606b9fad54a
Jose Juan R. Zuñiga <neodevelop@MBPJJRZ.local>
2009-03-10 21:16:36 -0600

    config in xml commented to use richui texteditor

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -12,7 +12,7 @@
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false
 grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
-                      //xml: ['text/xml', 'application/xml'],
+                      //xml: ['text/xml', 'application/xml'],//
                       text: 'text/plain',
                       js: 'text/javascript',
                       rss: 'application/rss+xml',
