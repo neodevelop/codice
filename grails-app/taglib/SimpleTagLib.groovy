c58ca2f2cc86d9d3dcfbbe89c9c0cd00751371ec
Jose Juan R. Zuñiga <neodevelop@MBPJJRZ.gateway.2wire.net>
2009-03-17 19:25:37 -0600

    evaluating the parameter, the first time when register one user the parameter is a ??string?? why? after is TimeStamp correct, also, the format of date is DayOfWeek Month etc., blabla and with the time stamp is yyyy-MM-dd

--- grails-app/taglib/SimpleTagLib.groovy
+++ grails-app/taglib/SimpleTagLib.groovy
@@ -1,5 +1,17 @@
+import java.text.SimpleDateFormat
+
 class SimpleTagLib {
 	def dateFormat = { attrs, body ->
-		out << new java.text.SimpleDateFormat(attrs.format).format(attrs.date)
+		if(attrs.date instanceof java.sql.Timestamp){
+			out << new SimpleDateFormat(attrs.format).format(attrs.date)
+		}
+		if(attrs.date instanceof String){
+			if(attrs.date =~ /^[A-Za-z]/){
+				out << new SimpleDateFormat(attrs.format).format(new Date())
+			}else{
+				def sdf = new SimpleDateFormat('yyyy-MM-dd H:m:s.S').parse(attrs.date)
+				out << new SimpleDateFormat(attrs.format).format(sdf)
+			}
+		}
 	}
 }
