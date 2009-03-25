e12abc80a9bcd648399a47d826b9b1d2c312f354
Jose Juan R. Zuñiga <neodevelop@MBPJJRZ.local>
2009-03-24 18:40:53 -0600

    priority property added in the command class

--- grails-app/controllers/com/synergyj/codice/content/ContentCommand.groovy
+++ grails-app/controllers/com/synergyj/codice/content/ContentCommand.groovy
@@ -17,10 +17,12 @@ class ContentCommand{
 	Boolean showInMainPage = true
 	String tagList
 	String contentType
+	Integer priority
 	
 	static constraints = {
 		email nullable:false,blank:false,email:true
 		title nullable:false,size:5..128,blank:false
 		textBody nullable:false,maxSize:10000,blank:false
+		priority min:-5,max:5
 	}
 }
