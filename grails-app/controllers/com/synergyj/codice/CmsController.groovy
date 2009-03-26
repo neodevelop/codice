f0734a1e890ec9badfc796792e303a517d8861c6
Jose Juan R. Zuñiga <neodevelop@MBPJJRZ.local>
2009-03-26 01:33:34 -0600

    refactor in size, we need another refactor...

--- grails-app/controllers/com/synergyj/codice/CmsController.groovy
+++ grails-app/controllers/com/synergyj/codice/CmsController.groovy
@@ -5,7 +5,7 @@ import com.synergyj.codice.content.*
 class CmsController {
     
     def index = { 
-		params.max = Math.min( params.max ? params.max.toInteger() : 3,  100)
+		params.max = Math.min( params.max ? params.max.toInteger() : 5,  100)
 		params.order = 'desc'
 		params.sort = 'created'
         [ lastContent: Content.list( params ) ]
