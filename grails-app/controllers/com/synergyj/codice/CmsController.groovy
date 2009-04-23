61dfed2b5c6649045bf99d051fe5591e5c67922f
Jose Juan R. Zuñiga <neodevelop@gmail.com>
2009-04-23 15:38:26 -0500

    using criteria at last...

--- grails-app/controllers/com/synergyj/codice/CmsController.groovy
+++ grails-app/controllers/com/synergyj/codice/CmsController.groovy
@@ -5,10 +5,14 @@ import com.synergyj.codice.content.*
 class CmsController {
     
     def index = { 
-		params.max = Math.min( params.max ? params.max.toInteger() : 5,  100)
-		params.order = 'asc'
-		params.sort = 'priority'
 		//here goes a criteria search
-        [ lastContent: Content.list( params ) ]
+		def criteria = Content.createCriteria()
+		def lastContent = criteria.list {
+			eq('publish',true)
+			maxResults(5)
+			order('priority','asc')
+			order('created','asc')
+		}
+        [ lastContent: lastContent ]
 	}
 }
