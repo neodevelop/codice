5c54ced07e792321a262a04fcbf35b7ddf050e8c
Jose Juan R. Zuñiga <neodevelop@gmail.com>
2009-04-23 14:43:18 -0500

    reorder the content in the main page...

--- grails-app/controllers/com/synergyj/codice/CmsController.groovy
+++ grails-app/controllers/com/synergyj/codice/CmsController.groovy
@@ -6,8 +6,8 @@ class CmsController {
     
     def index = { 
 		params.max = Math.min( params.max ? params.max.toInteger() : 5,  100)
-		params.order = 'desc'
-		params.sort = 'created'
+		params.order = 'asc'
+		params.sort = 'priority'
 		//here goes a criteria search
         [ lastContent: Content.list( params ) ]
 	}
